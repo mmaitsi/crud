f095f69a0420a5d242773b989096bc2b955e1c61
mmaitsi <kingsleyjere21@gmail.com>
2025-02-28 13:37:37 +0300

    added forms

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-
+    path('',views.user_list),
+    path('Add/',views.AddUser)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import User
+from .forms import UserForm
 
 # Create your views here.
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'listingpage.html', {'users':users})
+def AddUser(request):
+    mydict={}
+    form = UserForm(request.POST or None,request.FILES or None)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    mydict['form'] = form
+    return render(request, 'add.html', mydict)
